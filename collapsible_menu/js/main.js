fdf3cf8f1d07bbfaa4095f3d8244f22073268772
Evelina <andreeaevelinabigu@yahoo.co.uk>
2017-03-27 13:07:55 +0100

    Home_page_with_collapsible menu
    Collapsible menu with Bootstrap and JS

---
+++ collapsible_menu/js/main.js
@@ -0,0 +1,7 @@
+$('#accordion').on('hidden.bs.collapse', function () {
+})
+
+$('#accordion .accordion-toggle').click(function (e){
+  var chevState = $(e.target).siblings("i.indicator").toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+  $("i.indicator").not(chevState).removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+});
